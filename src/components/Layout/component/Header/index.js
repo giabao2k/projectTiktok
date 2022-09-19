76b55ad6fe95d5417875d3b932083b4ec8e11ef9
giabao2k <ngonguyengiabao2000@gmail.com>
2022-09-19 15:10:41 +0700

    dựng khung layout cho project

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,5 +1,14 @@
+// import classNames from 'classnames/bind';
+import styles from './Header.module.css';
+// sử dụng thư viện css bind của classnames
+// import styles from './Header.module.scss';
+// const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={styles.wrapper}>
+            <div className={styles.inner}>a</div>
+        </header>
+    );
 }
 
 export default Header;
