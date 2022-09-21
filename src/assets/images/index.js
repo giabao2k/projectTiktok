f71350d50f042d721385029279a33261df2ec558
giabao2k <ngonguyengiabao2000@gmail.com>
2022-09-21 10:05:22 +0700

    v1 header ui

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+const images = {
+    logo: require('~/assets/images/logo.svg'),
+    close: require('~/assets/images/close.svg'),
+    loading: require('~/assets/images/loading.svg'),
+    search: require('~/assets/images/search.svg'),
+};
+
+export default images;
--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,12 +1,33 @@
-// import classNames from 'classnames/bind';
 import styles from './Header.module.css';
+import images from '~/assets/images';
+// import classNames from 'classnames/bind';
 // sử dụng thư viện css bind của classnames
 // import styles from './Header.module.scss';
 // const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={styles.wrapper}>
-            <div className={styles.inner}>a</div>
+            <div className={styles.inner}>
+                <div className={styles.logo}>
+                    <img src={images.logo.default} alt="Tiktok" />
+                </div>
+                <div className={styles.search}>
+                    <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+                    <button className={styles.clearBtn}>
+                        <img src={images.close.default} />
+                    </button>
+                    <button className={styles.loadingBtn}>
+                        <img src={images.loading.default} />
+                    </button>
+                    <button className={styles.searchBtn}>
+                        <img src={images.search.default} />
+                    </button>
+                </div>
+                <div className={styles.actions}>
+                    <button className={styles.register}>Đăng kí</button>
+                    <button className={styles.login}>Đăng nhập</button>
+                </div>
+            </div>
         </header>
     );
 }
